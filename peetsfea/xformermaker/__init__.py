d6d86d8009315426c5ca164d57443fb02fac88d2
Harry Lee <glaysia@uos.ac.kr>
2025-07-09 21:59:35 +0900

    모듈 구조 실수 고침

--- peetsfea/xformermaker/__init__.py
+++ peetsfea/xformermaker/__init__.py
@@ -2,10 +2,12 @@
 from .xformermaker import XformerMakerInterface
 from .xformermaker import XformerType, XEnum
 from .xformermaker import seed as peets_global_rand_seed
+import project1
 
 
 __all__ = [
   "XformerMakerInterface",
   "XEnum", "XformerType",
-  "peets_global_rand_seed"
+  "peets_global_rand_seed",
+  "project1"
 ]
