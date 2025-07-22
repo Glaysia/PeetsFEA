45e6ab3471fb9276c2380d994d0ee714a581ba68
Harry Lee <glaysia@uos.ac.kr>
2025-07-22 15:07:17 +0900

    리팩토링

--- peetsfea/xformermaker/project1/__init__.py
+++ peetsfea/xformermaker/project1/__init__.py
@@ -1,5 +1,10 @@
 from .project1 import Project1_EE_Plana_Plana_2Series
+from .project2 import Project2
 from .winding_builder import WindingBuilder as WindingBuilder4Project1
 from .core_builder import CoreBuilder as CoreBuilder4Project1
-__all__ = ["Project1_EE_Plana_Plana_2Series",
-           "WindingBuilder4Project1", "CoreBuilder4Project1"]
+__all__ = [
+  "Project1_EE_Plana_Plana_2Series",
+  "WindingBuilder4Project1",
+  "CoreBuilder4Project1",
+  "Project2"
+]
