499725e14462cb9012b9862184be287626a7a7e0
Harry Lee <glaysia@uos.ac.kr>
2025-08-04 18:49:02 +0900

    v0.2.0 시작

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 
 setuptools.setup(
     name="PeetsFEA",                                      # PyPI package name
-    version="0.1.9",                                      # release version
+    version="0.2.0",                                      # release version
     author="Glaysia",                                     # author name
     author_email="willbecat27@gmail.com",                 # author email
     description="전력전자 자동화에 필요한 유틸리티 모듈 모음",  # short description
@@ -22,13 +22,10 @@ setuptools.setup(
     package_dir={"peetsfea": "peetsfea"},
 
     # supported Python versions
-    python_requires=">=3.10",
+    python_requires=">=3.13",
     install_requires=[                                    # dependencies with platform markers
         "pyaedt==0.17.4",                                 # AEDT Python API
-        "pycaret==3.3.2",                                 # data science library
         "ipykernel>=6.30.0",                              # Jupyter kernel for Python
-        "torch==2.7.1",                                   # PyTorch for machine learning
-        "seaborn==0.13.2",                                # statistical data visualization
     ],
 
     classifiers=[                                         # PyPI metadata
